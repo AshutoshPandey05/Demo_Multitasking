e3492cf8caa08b6bb9936e728a1b5b3e3c92dc57
Ashutosh Kr Pandey <92009208+AshutoshPandey05@users.noreply.github.com>
2021-10-18 13:09:47 +0530

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Multitasking
+{
+    class MyClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Implementing thread 1");
+            for(int i = 0; i<100; i++)
+            {
+                Console.WriteLine("\n {0}", i);
+            }
+            Console.WriteLine("Thread 1 Closes");
+        }
+        public void Read()
+        {
+            Console.WriteLine("Implementing Thread 2...");
+            for(char c= 'A'; c<='Z'; c++)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("Thread 2 Closes");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+
+namespace Demo_Multitasking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing Multitaskin using Multithreading in C#");
+            MyClass History = new MyClass();
+            Console.WriteLine("Main Thread Started....!!");
+            History.Display();
+
+
+            ThreadStart th1 = History.Display;
+            ThreadStart th2 = History.Read;
+            Thread s1 = new Thread(th1);
+            Thread s2 = new Thread(th2);
+            s1.Start();
+            s2.Start();
+
+            Console.WriteLine("Main Thread ends here...");
+            Console.ReadKey();
+            
+        }
+    }
+}
